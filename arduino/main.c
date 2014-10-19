bc686414ad7befb078932b5e22a8f73a13f6008c
TW <tomasz.wisni3wski@gmail.com>
2014-10-19 21:24:29 +0100

    LCD output selected during the compilation

--- arduino/main.c
+++ arduino/main.c
@@ -5,13 +5,18 @@
 #include "pca.h"
 #include "nega.h"
 
+#define LCD_STDOUT 0
+
+
 int main(void)
 {
 	grid_t grid;
 	int8_t result = 0;
 
+#if LCD_STDOUT == 1
 	struct dev_pcd8544_ctx lcd;
 	struct bus_t spi_bus;
+#endif
 
 	serial_init(E_BAUD_4800);	
 	serial_install_interrupts(E_FLAGS_SERIAL_RX_INTERRUPT);
@@ -19,6 +24,7 @@ int main(void)
 
 	serial_install_stdio();
 
+#if LCD_STDOUT == 1
 	spi_bus = spi_hw_poll_bus_get();
 	lcd.bus = &spi_bus;
 
@@ -33,22 +39,30 @@ int main(void)
 	pcd8544_init(&lcd);
 	pcd8544_clrscr(&lcd);
 	pcd8544_install_stdout(&lcd);
+#endif
 
 	while (1) {
+#if LCD_STDOUT == 1
 		pcd8544_clrscr(&lcd);
+#endif
 		printf("Let's play a game...\n");
 		memset(&grid, 0x00, sizeof(grid_t));
 		dump_grid(&grid, 1);
 
 		while (1) {
 			get_human_move(&grid, CROSS);
+
+#if LCD_STDOUT == 1
 			pcd8544_clrscr(&lcd);
+#endif
 			dump_grid(&grid, 1);
 
 			nega_max(&grid, 0, CIRCLE);
 			grid.g[get_best_move()] = CIRCLE;
 
+#if LCD_STDOUT == 1
 			pcd8544_clrscr(&lcd);
+#endif
 			printf("CPU\n");
 			dump_grid(&grid, 1);
 
